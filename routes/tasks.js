0778c5df24de0d93f445bc9a550e4d22bcea0c20
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-05-26 01:10:50 +0200

    add

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.route('/').get((req, res) => {
-    res.send('all items')
-})
+const {getAllTasks} = require('../controllers/tasks')
+
+router.route('/').get(getAllTasks)
 
 module.exports = router

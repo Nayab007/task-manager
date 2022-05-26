06c71ee472b73dddc68da8f4344a9c4ae6a7631e
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-05-27 00:42:08 +0200

    add

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const port = 3000;
 
 const start = async () => {
   try {
-    await connectDB()
+    await connectDB(process.env.MONGO_URL)
     app.listen(port, () => 
     console.log(`Server is on port ${port}...`)
     );

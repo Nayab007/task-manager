0f2eb5833a2a9530ffcd3eceba519a88f617f363
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-05-27 00:39:17 +0200

    add

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
 
-const connectionString = 
-'mongodb+srv://nayab:MAFpgJV21sZ8ELx2@nodeexpresspoject.qrurrdq.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority'
 
 
 const connectDB = (url) => {
